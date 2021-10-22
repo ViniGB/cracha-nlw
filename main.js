56a2325c6f6f1ac89a49f1320fa5daac3002b20c
Vinicius Gomes <92736867+ViniGB@users.noreply.github.com>
2021-10-21 23:28:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const LinksSocialMedia = {
+  github: 'ViniGB',
+  youtube: 'UCiJzl-nQpGC0FZPEOYtxHzg',
+  facebook: 'vinicius.gomes.52',
+  instagram: 'biniciusgomes',
+  twitter: 'ViniSemoG'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
+
+//ARROW FUNCTION
+//function nomeDaFuncao(argumento){}
+//code
+
+//argumento => {}
